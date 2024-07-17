144e7eed5ef6ab8869d743296148cf5b7c13ead9
SrikanthChincholi <124526647+SrikanthChincholi@users.noreply.github.com>
2024-07-17 19:20:34 +0530

    Update TestClass.java
    added logic to the class

--- AshokIT_3/src/test/java/AshokIT_3/AshokIT_3/TestClass.java
+++ AshokIT_3/src/test/java/AshokIT_3/AshokIT_3/TestClass.java
@@ -3,6 +3,14 @@ package AshokIT_3.AshokIT_3;
 public class TestClass {
 	
 	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+
+		a = a + b;
+		a= a - b;
+		b = a - b ;
+		System.out.println("a value "+ a + "b value " + b);
 		
 	}
 
