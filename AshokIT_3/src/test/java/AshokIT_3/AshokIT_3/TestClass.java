3dc8482c53bf74b596fe5d83ac07be3d3f123998
srikanthchincholi <srikanthchincholi@yahoo.com>
2024-07-17 19:22:13 +0530

    updated logic

--- AshokIT_3/src/test/java/AshokIT_3/AshokIT_3/TestClass.java
+++ AshokIT_3/src/test/java/AshokIT_3/AshokIT_3/TestClass.java
@@ -1,17 +1,17 @@
 package AshokIT_3.AshokIT_3;
 
 public class TestClass {
-	
+
 	public static void main(String[] args) {
 
 		int a = 10;
 		int b = 20;
 
 		a = a + b;
-		a= a - b;
-		b = a - b ;
-		System.out.println("a value "+ a + "b value " + b);
-		
+		b = a - b;
+		a = a - b;
+		System.out.println("a value " + a + " b value " + b);
+
 	}
 
 }
