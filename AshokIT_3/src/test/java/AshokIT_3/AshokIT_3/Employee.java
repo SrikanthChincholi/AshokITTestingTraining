7730e68cd73cc18b291a2ef45bf40de733c91e32
srikanthchincholi <srikanthchincholi@yahoo.com>
2024-07-18 21:04:43 +0530

    Updated code

--- AshokIT_3/src/test/java/AshokIT_3/AshokIT_3/Employee.java
+++ AshokIT_3/src/test/java/AshokIT_3/AshokIT_3/Employee.java
@@ -5,36 +5,34 @@ public class Employee {
 	String name;
 	int empno;
 	String designation;
-	Bonus bonus;
-	
+	static Bonus bonus;
+
 	public Employee(String name, int empno, String designation, Bonus bonus) {
 		this.name = name;
 		this.empno = empno;
 		this.designation = designation;
-		this.bonus = bonus;
+		Employee.bonus = bonus;
 	}
+
 	public static void main(String[] args) {
-		Bonus bonus = new Bonus(12000.00f, 45000.00f);
-		Employee emp = new Employee("Srikanth", 123, "Qa Lead", bonus);
-		emp.totalsalary();
+		bonus = new Bonus(12000.00f, 45000.00f);
+		new Employee("Srikanth", 123, "Qa Lead", bonus);
+		bonus.totalsalary();
 	}
-	
-
-	public void totalsalary() {
-		float totalsal = (bonus.sal + bonus.bonustoemp);
-		System.out.println("Total salary after bonus : " + totalsal);
-	}
-
 }
 
 class Bonus {
-	
+
 	float bonustoemp;
 	float sal;
-	
 
 	public Bonus(float bonus, float sal) {
 		this.bonustoemp = bonus;
 		this.sal = sal;
+	}
+
+	public void totalsalary() {
+		float totalsal = (sal + bonustoemp);
+		System.out.println("Total salary after bonus : " + totalsal);
 	}
 }
